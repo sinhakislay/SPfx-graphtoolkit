bb193086c790b5353701f56b28be77e3e32d1da6
Kislay Sinha <kislay_sinha@outlook.com>
2020-12-28 14:37:46 +0100

    Update SpfxMgtWebPart.ts

--- src/webparts/spfxMgt/SpfxMgtWebPart.ts
+++ src/webparts/spfxMgt/SpfxMgtWebPart.ts
@@ -31,28 +31,10 @@ export default class SpfxMgtWebPart extends BaseClientSideWebPart<ISpfxMgtWebPar
     <button class="button">Get SelectedChannel</button>
     <div class="output"></div>  
    `;
-  this._setButtonEventHandlers();
+  
   }
 
-  private _setButtonEventHandlers(): void {  
-    //const webPart: SpfxMgtWebPart = this;  
   
-    document.querySelector('.button').addEventListener('click', _ => {
-      const picker = document.querySelector('mgt-teams-channel-picker');
-      const output = document.querySelector('.output');
-  
-      if (picker.selectedItem) {
-          output.innerHTML = '<b>channel:</b> ' + picker.selectedItem.channel.displayName;
-          output.innerHTML += '<br/><b>team:</b> ' + picker.selectedItem.team.displayName;
-      } else {
-          output.innerHTML = 'no channel selected';
-      }
-});
-       
-         
-    });
-   
-     
     }
   //protected get dataVersion(): Version {
     //return Version.parse('1.0');
